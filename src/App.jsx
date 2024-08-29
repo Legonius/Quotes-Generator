c2f16e6b848c50613f2494f43406fda832d94c45
Legonius <emoinblackzone@gmail.com>
2024-08-30 01:11:04 +0630

    upload finished project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/card";
+
+function App() {
+  return (
+    <div className="box">
+      <h1>Quotes Generator Project</h1>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { quotes } from "../assets/quotes";
+
+const Card = () => {
+  const [quote, setQuote] = useState(quotes[0]);
+  const handleClick = () => {
+    const randomNumber = Math.floor(Math.random() * quotes.length) - 1;
+    setQuote(quotes[randomNumber]);
+  };
+
+  return (
+    <div
+      id="quote-box"
+      className="card"
+      style={{ minWidth: " 26rem", maxWidth: "30rem" }}
+    >
+      <div className="card-body">
+        <h5 id="author" className="card-title">
+          {quote ? quote.author : ""}
+        </h5>
+        <h6 className="card-subtitle mb-2 text-black-50">
+          {quote && quote.profession}
+        </h6>
+        <p id="text" className="card-text">
+          {quote && quote.quote}
+        </p>
+        <div className="d-flex justify-content-between">
+          <button
+            onClick={(e) => handleClick()}
+            id="new-quote"
+            className="btn btn-outline-primary"
+          >
+            New Quote
+          </button>
+          <a
+            id="tweet-quote"
+            href="twitter.com/intent/tweet"
+            target="_blank"
+            className="btn btn-outline-secondary"
+          >
+            Tweet
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
